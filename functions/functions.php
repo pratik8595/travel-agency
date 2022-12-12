1e85cf3d8bbf783f41c5daf16dc7c7221b2ec051
pratik8595 <116726387+pratik8595@users.noreply.github.com>
2022-12-12 15:11:19 +0530

    Update functions.php
    function folder edited

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "tagency");
+$con = mysqli_connect("database-2.cqdbhospaxgw.ap-northeast-1.rds.amazonaws.com", "admin:admin123", "", "database-2");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
