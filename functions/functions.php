98eb39556d9c84c6402ddcbcb04cff7b2372d58c
pratik8595 <116726387+pratik8595@users.noreply.github.com>
2022-12-13 19:24:13 +0530

    updated

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("database-2.cqdbhospaxgw.ap-northeast-1.rds.amazonaws.com", "admin:admin123", "", "pratik");
+$con = mysqli_connect("database-2.cqdbhospaxgw.ap-northeast-1.rds.amazonaws.com", "admin:admin123", "", "tangency");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
